c9cc7398a69eac1fc61c0b52754be3e2026afbc8
JP Rose <test@test.com>
2023-04-27 17:05:26 -0400

    fix img path des nationalites

--- src/components/ui/CardTheme.jsx
+++ src/components/ui/CardTheme.jsx
@@ -1,7 +1,7 @@
 const CardTheme = ({ id, nom, img, description}) => {
     return ( 
         <div className="shadow-lg bg-white self-start">
-            <div className="py-16 bg-center bg-no-repeat bg-cover" style={{ "backgroundImage": `url('${img}')` }}>
+            <div className="py-16 bg-center bg-no-repeat bg-cover" style={{ "backgroundImage": `url('/idees-recettes${img}')` }}>
                 <p className="w-full bg-white/80 text-center p-4 text-stone-800 text-2xl uppercase font-normal">{ nom }</p>
             </div>
             { description && <div className="text-stone-800 text-sm p-4 leading-tight">
